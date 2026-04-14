50227a606456aed9ec22803b23328cad9d94e15a
ashijain2006 <ashijain2006@gmail.com>
2026-04-14 14:18:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,10 +20,11 @@ model = timm.create_model(
 )
 
 import gdown
+import os
 
 if not os.path.exists("model.pth"):
-    url = "https://drive.google.com/uc?id=1K2GDj9DV-Cz6mUMSMf-1m2Pq_i0_2y0"
-    gdown.download(url, "model.pth", quiet=False)
+    url = "https://drive.google.com/uc?id=1K2GDj9DV-Cz6mUMSMf-1m2Pq_i0_2y0_"
+    gdown.download(url, "model.pth", quiet=False, fuzzy=True, use_cookies=False)
 
 model.load_state_dict(torch.load("model.pth", map_location=device))
 model.to(device)
